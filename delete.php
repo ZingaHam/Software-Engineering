cae6fa8ede6b4d240c04883260beab5f9ab87f85
Madison-LH <60183961+Madison-LH@users.noreply.github.com>
2022-11-22 13:56:43 -0500

    changed how redirection is handled again
    same process as what was done for uploads

--- delete.php
+++ delete.php
@@ -1,13 +1,7 @@
 <?php
- 
+//automatic redirection back to the landing page by specifying it in a header
+header("location:index.php"); 
 // Built-in PHP function to delete file
 unlink($_GET["name"]);
  
-// Redirecting back
-if(isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] != ""){
-$url = $_SERVER['HTTP_REFERER'];
-}else{
-$url = "index.php";
-}
 
-header("Location: ".$url);;
