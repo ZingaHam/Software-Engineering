8c1064cb5095d2b07461ef0b6d5521ec5ae406b9
Madison-LH <60183961+Madison-LH@users.noreply.github.com>
2022-11-22 13:55:39 -0500

    changed redirection again
    removed any $_SERVER variables or HTTP referrals and now utilizing headers for redirection

--- upload.php
+++ upload.php
@@ -1,16 +1,9 @@
 <?php
- 
+//automatic redirection back to landing page by using a header
+header("location:index.php"); 
 // Getting uploaded file
 $file = $_FILES["file"];
  
 // Uploading in "uploads" folder
 move_uploaded_file($file["tmp_name"], "uploads/" . $file["name"]);
  
-// Redirecting back
-if(isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] != ""){
-$url = $_SERVER['HTTP_REFERER'];
-}else{
-$url = "index.php";
-}
-
-header("Location: ".$url);;
