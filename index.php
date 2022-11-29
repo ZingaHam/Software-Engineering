d2375c530963b0b984706f1fedebc37ccdfb7669
N'Zinga Hammonds-Wyatt <nzingahammondswyatt@gmail.com>
2022-11-29 17:51:04 -0500

    Update index.php
    - Corrected JS function

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <input type = "submit" value = " Submit "/><br />
     </form>
     <p>Or create a New Account by Clicking the Button Below</p>
-    <button onclick= "window.location.href('new_account.php')">Create Account</button>
+    <button onclick= "window.location.replace('new_account.php')">Create Account</button>
 </div>
 
 </body>
