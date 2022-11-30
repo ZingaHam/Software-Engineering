8859bcbf6afd6a8519df9a14e392e749f17d0b8e
N'Zinga Hammonds-Wyatt <nzingahammondswyatt@gmail.com>
2022-11-29 19:36:39 -0500

    Update CONFIG.php
    -updating IP address

--- CONFIG.php
+++ CONFIG.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_SERVER', 'ip-172-31-86-95.ec2.internal:3306');
+define('DB_SERVER', '172.31.86.95:3306');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'CS_Seniors23');
 define('DB_DATABASE', 'site_data');
