fc56ac2867acffcc57c4d53447ef0100fd6af090
N'Zinga Hammonds-Wyatt <nzingahammondswyatt@gmail.com>
2022-11-29 19:13:12 -0500

    Update CONFIG.php
    - adding connection validation

--- CONFIG.php
+++ CONFIG.php
@@ -4,4 +4,10 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'CS_Seniors23');
 define('DB_DATABASE', 'site_data');
 $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+// Check connection
+if (!$db){
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
