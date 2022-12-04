f9d57a9c8328bdf673daf8e5b6df7de8963da7e8
N'Zinga Hammonds-Wyatt <nzingahammondswyatt@gmail.com>
2022-12-03 23:34:34 -0500

    SQL Fix 1
    - attempt at SQL fix by changing connection commands

--- CONFIG.php
+++ CONFIG.php
@@ -3,10 +3,10 @@ define('DB_SERVER', '172.31.86.95:3306');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'CS_Seniors23');
 define('DB_DATABASE', 'site_data');
-$db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die(mysqli_error());
 
 // Check connection
-if (!$db){
+if (empty(mysqli_error())){
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
